812d1f520166356e77827b86527f8343074b9b72
jjunmo <yymeyy2002@gmail.com>
2022-12-07 13:56:35 +0900

    READING 문제 갯수 수정

--- src/main/java/com/example/jkedudemo/module/common/enums/exam/Exam.java
+++ src/main/java/com/example/jkedudemo/module/common/enums/exam/Exam.java
@@ -3,7 +3,7 @@ package com.example.jkedudemo.module.common.enums.exam;
 public enum Exam {
     //ExamCategory
     //TODO: 문제 개수 이후 수정
-    SPEAKING(5) ,READING(5) ,LISTENING(5), GRAMMAR(5), WRITING(5);
+    SPEAKING(5) ,READING(34) ,LISTENING(5), GRAMMAR(5), WRITING(5);
 
     private final int value;
 
